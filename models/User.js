1bf6ca4f20a14480e83ba72e068c4b3c467dcfad
Aragor70 <mikis3@o2.pl>
2020-08-26 22:17:24 +0100

    public repository

--- models/User.js
+++ models/User.js
@@ -1,3 +1,4 @@
+const crypto = require('crypto');
 const mongoose = require('mongoose');
 const UserSchema = new mongoose.Schema({
     
@@ -17,11 +18,25 @@ const UserSchema = new mongoose.Schema({
     avatar: {
         type: String
     },
+    resetPasswordToken: String,
+    resetPasswordExpire: Date,
     date: {
         type: Date,
         default: Date.now
     }
     
 });
+
+UserSchema.methods.getResetPasswordToken = function() {
+    // generate token
+    let resetToken = crypto.randomBytes(20).toString('hex');
+    console.log(crypto.randomBytes(20))
+    // hash token and put it to resetPasswordToken in the model
+    this.resetPasswordToken = crypto.createHash('sha256').update(resetToken).digest('hex');
+
+    // set expire
+    this.resetPasswordExpire = Date.now() + 10 * 60 * 1000;
+    return resetToken;
+}
 
 module.exports = User = mongoose.model('user', UserSchema);
