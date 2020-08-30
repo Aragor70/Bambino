a02542a6861a4ef4e19101eebe428fe8ee4b83f0
Aragor70 <mikis3@o2.pl>
2020-08-30 22:02:11 +0100

    files uploading update

--- routes/api/authors.js
+++ routes/api/authors.js
@@ -191,7 +191,7 @@ router.post('/image/:id', auth, async(req, res) => {
         }
         await author.images.unshift(newImage);
         
-        file.mv(`./client/uploads/authors/image/${file.name}`, err => {
+        file.mv(`./build/uploads/authors/image/${file.name}`, err => {
             if(err){
                 console.error(err);
                 return res.status(500).send(err);
