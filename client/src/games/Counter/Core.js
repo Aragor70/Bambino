d796e86c048b275d66511ecdc18a78e4e1e59f19
Aragor70 <mikis3@o2.pl>
2020-09-29 02:37:17 +0100

    public game

--- client/src/games/Counter/Core.js
+++ client/src/games/Counter/Core.js
@@ -2,25 +2,28 @@ import React, { Fragment, useState } from 'react'
 
 
 const Core = () => {
+
     const [play, setPlay] = useState(false)
 
     const [score, setScore] = useState(0)
 
-    const [tasks, setTasks] = useState({
+    const [tasks, setTasks] = useState([{
         quest: '',
         answer: ''
-    })
+    }])
+    const [userAnswer, setUserAnswer] = useState('')
+
     const handleChange = (e) => {
-        
+        setUserAnswer(e.target.value)
     }
     const [lvl, setLvl] = useState(1)
 
-    const start = () => {
+    const start = (lvl) => {
         var values = []
         for (let i = 0; values.length < 5; i++ ) {
-            const x = generator(11)
-            const y = generator(11)
-            if( x + y <= 10 ) {
+            const x = generator(lvl * 10 + 1)
+            const y = generator(lvl * 10 + 1)
+            if( x + y <= lvl * 10 ) {
                 values.push({
                     quest: `${x} + ${y}`,
                     answer: x + y
@@ -33,32 +36,52 @@ const Core = () => {
     const generator = (max) => {
         return Math.floor(Math.random() * max)  
     }
+    if(userAnswer && userAnswer == tasks[0].answer) {
+        setTimeout(() => {
+            
+            setUserAnswer('')
+            
+            setTasks(tasks.slice(1))
+            setScore(score + 1)
 
-    console.log(tasks)
+        }, 500)
+        console.log("correct")
+        
+        
+    }
+        
+    if(play && tasks == false) {
+        setLvl(lvl + 1)
+        
+        return start(lvl + 1)
+            
+    }
+    
+    
+
     return (
         <Fragment>
             <div className="count-wrapper">
                 
                 {
-                    !play && <button type="button" onClick={e=> {setPlay(true), start()}}>start</button>
+                    !play && <button type="button" className="countBtn" onClick={e=> {setPlay(true), start(lvl)}}>start</button>
                 }
                 
-
                 {
                     play && <Fragment>
                         <div className="task-list">
-                        {
-                            tasks.map(task => <Fragment>
-                                <div className="task">{task.quest}
-                                </div>
-
-                            </Fragment>) 
-                        }
+                            
+                                <div className="task">{tasks[0].quest}</div>
                         
-                        <input type="text" className="user-input" onChange={e => handleChange(e)} />
+                        <input type="text" className="user-input" value={userAnswer} onChange={e => handleChange(e)} />
+                        
                         </div>
+                        
+                <h1>level: {lvl}</h1>
+                <h1>score: {score}</h1>
                     </Fragment>
                 }
+                
             </div>
         </Fragment>
     )
