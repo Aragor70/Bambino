89c8ac81ef5f5860b198c3c99a823067c5f13d1f
Aragor70 <mikis3@o2.pl>
2020-10-17 13:24:42 +0100

    public notifies

--- client/src/userAccount/Message.js
+++ client/src/userAccount/Message.js
@@ -6,6 +6,10 @@ import client from '../reducers/client';
 
 const Message = ({message, seeMessage, index, messages, match, client, user }) => {
 
+    useEffect(() => {
+        seeMessage(message._id)
+    }, [])
+    
     let obj = document.getElementById(`scrollTo${messages.length}`)
     useEffect(()=> {
         
@@ -14,9 +18,7 @@ const Message = ({message, seeMessage, index, messages, match, client, user }) =
         
     }, [obj, match.params.id, match.params.x])
 
-    useEffect(() => {
-        seeMessage(message._id)
-    }, [])
+    
 
     console.log(message)
 
@@ -24,19 +26,20 @@ const Message = ({message, seeMessage, index, messages, match, client, user }) =
             <Fragment>
                 <div className="full-width-message">
                 {
-                    message.user._id == client._id && <Fragment>
+                    user._id !== client._id && message.user._id == client._id && <Fragment>
                         <div className="message-row" id={`scrollTo${index + 1}`}>
                         <span className="message-name">{client.name} </span> <span className="message-txt">{message.text}</span>
                         </div>
-                    </Fragment> 
+                    </Fragment>
                 }
                 {
-                    message.user._id == user._id && <Fragment>
+                    user._id !== client._id && message.user._id == user._id && <Fragment>
                     <div className="message-row user-message" id={`scrollTo${index + 1}`}>
                         <span className="message-name">{user.name} </span> <span className="message-txt">{message.text}</span>
                         </div>
                         </Fragment>
                 }
+                
                 </div>
                 
             </Fragment>
